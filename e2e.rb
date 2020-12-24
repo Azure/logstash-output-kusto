a3953d7c515043702fb95e4580df677310b45fda
Asaf Mahlev <asafmahlev@microsoft.com>
2020-12-24 09:14:22 +0200

    Fixed shell command

--- e2e.rb
+++ e2e.rb
@@ -1,2 +1,2 @@
-puts %(/usr/share/logstash/bin/logstash --version)
-puts %(/usr/share/logstash/bin/logstash-plugin list)
+puts %x(/usr/share/logstash/bin/logstash --version)
+puts %x(/usr/share/logstash/bin/logstash-plugin list)
