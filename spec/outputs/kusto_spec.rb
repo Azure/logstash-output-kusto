5cab14719b8dee5af0973a9543ffc238127bc8da
MonishkaDas <monishka29.05@gmail.com>
2024-09-25 15:07:40 +0530

    Updated kusto_spec.rb and ingestor_spec.rb
    Testing spec files

--- spec/outputs/kusto_spec.rb
+++ spec/outputs/kusto_spec.rb
@@ -20,52 +20,6 @@ describe LogStash::Outputs::Kusto do
     "max_interval" => 10
   } }
   
-  describe '#register' do
-    it 'allows valid configuration' do
-      RSpec.configuration.reporter.message("Running test: allows valid configuration")
-      kusto = described_class.new(options)
-      expect { kusto.register }.not_to raise_error
-      kusto.close
-      RSpec.configuration.reporter.message("Completed test: allows valid configuration")
-    end
-  end
-
-  describe '#multi_receive_encoded' do
-    it 'buffers events and flushes based on max_size' do
-      RSpec.configuration.reporter.message("Running test: buffers events and flushes based on max_size")
-      kusto = described_class.new(options.merge({'max_size' => 2}))
-      kusto.register
-
-      event1 = LogStash::Event.new("message" => "event1")
-      event2 = LogStash::Event.new("message" => "event2")
-      event3 = LogStash::Event.new("message" => "event3")
-
-      expect(kusto.instance_variable_get(:@buffer)).to receive(:flush).twice.and_call_original
-
-      kusto.multi_receive_encoded([[event1, event1.to_json], [event2, event2.to_json]])
-      kusto.multi_receive_encoded([[event3, event3.to_json]])
-
-      kusto.close
-      RSpec.configuration.reporter.message("Completed test: buffers events and flushes based on max_size")
-    end
-
-    it 'flushes events based on max_interval' do
-      RSpec.configuration.reporter.message("Running test: flushes events based on max_interval")
-      kusto = described_class.new(options.merge({'max_interval' => 1}))
-      kusto.register
-
-      event1 = LogStash::Event.new("message" => "event1")
-
-      expect(kusto.instance_variable_get(:@buffer)).to receive(:flush).at_least(:once).and_call_original
-
-      kusto.multi_receive_encoded([[event1, event1.to_json]])
-
-      sleep 2
-
-      kusto.close
-      RSpec.configuration.reporter.message("Completed test: flushes events based on max_interval")
-    end
-  end
 
   describe '#close' do
     it 'shuts down the buffer and ingestor' do
