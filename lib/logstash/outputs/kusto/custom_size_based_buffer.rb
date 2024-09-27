76904bd8c31961771c92daed9de0124fff32a0bd
MonishkaDas <monishka29.05@gmail.com>
2024-09-27 14:18:27 +0530

    Updated max_size config
    The max_size config now refers to the size of the buffer (defaults to
    10MB) instead of number of events in the buffer

--- lib/logstash/outputs/kusto/custom_size_based_buffer.rb
+++ lib/logstash/outputs/kusto/custom_size_based_buffer.rb
@@ -4,10 +4,11 @@ module LogStash
   module Outputs
     class CustomSizeBasedBuffer
       def initialize(max_size, max_interval, &flush_callback)
-        @max_size = max_size
+        @max_size = max_size * 1024 * 1024 # Convert MB to bytes
         @max_interval = max_interval
         @flush_callback = flush_callback
         @buffer = []
+        @buffer_size = 0 # Initialize buffer size
         @mutex = Mutex.new
         @last_flush_time = Time.now
         @shutdown = false
@@ -22,9 +23,11 @@ module LogStash
 
       def <<(event)
         @mutex.synchronize do
+          event_size = event.bytesize
           @buffer << event
-          if @buffer.size >= @max_size
-            @logger.debug("Size-based flush triggered after #{@max_size} was reached")
+          @buffer_size += event_size
+          if @buffer_size >= @max_size
+            @logger.debug("Size-based flush triggered after #{@max_size} bytes was reached")
             flush
           end
         end
@@ -70,7 +73,7 @@ module LogStash
         return if @buffer.empty?
 
         begin
-          @logger.debug("Flushing buffer with #{@buffer.size} events")
+          @logger.debug("Flushing buffer with #{@buffer.size} events and #{@buffer_size} bytes")
           @flush_callback.call(@buffer)
         rescue => e
           # Log the error and continue,
@@ -78,6 +81,7 @@ module LogStash
           @logger.error(e.backtrace.join("\n"))
         ensure
           @buffer.clear
+          @buffer_size = 0 # Reset buffer size
           @last_flush_time = Time.now
         end
       end
