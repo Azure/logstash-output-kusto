f560b093136ae006c1058073581f261b078052f0
MonishkaDas <monishka29.05@gmail.com>
2024-10-27 15:42:21 +0530

    Added max_retries and failed_items_path() configs
    Updated buffer_flush and upload()

--- lib/logstash/outputs/kusto/custom_size_based_buffer.rb
+++ lib/logstash/outputs/kusto/custom_size_based_buffer.rb
@@ -1,82 +1,74 @@
 require 'logger'
 require 'thread'
-require 'fileutils'
-require 'securerandom'
+require 'csv'
 
 module LogStash
   module Outputs
     class CustomSizeBasedBuffer
-      def initialize(max_size_mb, max_interval, &flush_callback)
+      def initialize(max_size_mb = 10, max_interval = 10, max_retries = 3, failed_items_path = nil, &flush_callback)
         @buffer_config = {
           max_size: max_size_mb * 1024 * 1024, # Convert MB to bytes
           max_interval: max_interval,
-          buffer_dir: './tmp/buffer_storage/',
+          max_retries: max_retries,
+          failed_items_path: failed_items_path,
           logger: Logger.new(STDOUT)
         }
         @buffer_state = {
           pending_items: [],
           pending_size: 0,
           last_flush: Time.now.to_i,
-          timer: nil,
-          network_down: false
-        }
-        @flush_callback = flush_callback
-        @shutdown = false
-        @pending_mutex = Mutex.new
-        @flush_mutex = Mutex.new
-        load_buffer_from_files
-        @buffer_config[:logger].info("CustomSizeBasedBuffer initialized with max_size: #{max_size_mb} MB, max_interval: #{max_interval} seconds")
-      
-        # Start the timer thread after a delay to ensure initializations are completed
-        Thread.new do
-          sleep(10) 
-          @buffer_state[:timer] = Thread.new do
+          timer: Thread.new do
             loop do
               sleep(@buffer_config[:max_interval])
               buffer_flush(force: true)
             end
           end
-        end
+        }
+        @flush_callback = flush_callback
+        @pending_mutex = Mutex.new
+        @flush_mutex = Mutex.new
+        @buffer_config[:logger].info("CustomSizeBasedBuffer initialized with max_size: #{max_size_mb} MB, max_interval: #{max_interval} seconds, max_retries: #{max_retries}, failed_items_path: #{failed_items_path}")
       end
 
-				def <<(event)
-					while buffer_full? do
-						sleep 0.1
-					end
+      def <<(event)
+        while buffer_full? do
+          sleep 0.1
+        end
 
         @pending_mutex.synchronize do
           @buffer_state[:pending_items] << event
           @buffer_state[:pending_size] += event.bytesize
         end
+
+        # Trigger a flush if the buffer size exceeds the maximum size
+        if buffer_full?
+          buffer_flush(force: true)
+        end
       end
 
       def shutdown
         @buffer_config[:logger].info("Shutting down buffer")
-        @shutdown = true
         @buffer_state[:timer].kill
         buffer_flush(final: true)
-        clear_buffer_files
       end
 
-				private
+      private
 
-				def buffer_full?
-					@pending_mutex.synchronize do
-						@buffer_state[:pending_size] >= @buffer_config[:max_size]
-					end
-				end
+      def buffer_full?
+        @pending_mutex.synchronize do
+          @buffer_state[:pending_size] >= @buffer_config[:max_size]
+        end
+      end
 
-				def buffer_flush(options = {})
-					force = options[:force] || options[:final]
-					final = options[:final]
+      def buffer_flush(options = {})
+        force = options[:force] || options[:final]
+        final = options[:final]
 
-					if final
-						@flush_mutex.lock
-					elsif !@flush_mutex.try_lock
-						return 0
-					end
-
-        items_flushed = 0
+        if final
+          @flush_mutex.lock
+        elsif !@flush_mutex.try_lock
+          return 0
+        end
 
         begin
           outgoing_items = []
@@ -84,96 +76,68 @@ module LogStash
 
           @pending_mutex.synchronize do
             return 0 if @buffer_state[:pending_size] == 0
-
-							time_since_last_flush = Time.now.to_i - @buffer_state[:last_flush]
+            time_since_last_flush = Time.now.to_i - @buffer_state[:last_flush]
 
             if !force && @buffer_state[:pending_size] < @buffer_config[:max_size] && time_since_last_flush < @buffer_config[:max_interval]
               return 0
             end
 
             if force
-              @buffer_config[:logger].info("Time-based flush triggered after #{@buffer_config[:max_interval]} seconds")
-            elsif @buffer_state[:pending_size] >= @buffer_config[:max_size]
-              @buffer_config[:logger].info("Size-based flush triggered at #{@buffer_state[:pending_size]} bytes was reached")
-            else
-              @buffer_config[:logger].info("Flush triggered without specific condition")
+              if time_since_last_flush >= @buffer_config[:max_interval]
+                @buffer_config[:logger].info("Time-based flush triggered after #{@buffer_config[:max_interval]} seconds")
+              else
+                @buffer_config[:logger].info("Size-based flush triggered when #{@buffer_state[:pending_size]} bytes was reached")
+              end
             end
 
             outgoing_items = @buffer_state[:pending_items].dup
             outgoing_size = @buffer_state[:pending_size]
-            @buffer_state[:pending_items] = []
-            @buffer_state[:pending_size] = 0
+            buffer_initialize
           end
 
+          retries = 0
           begin
+            @buffer_config[:logger].info("Flushing: #{outgoing_items.size} items and #{outgoing_size} bytes to the network")
             @flush_callback.call(outgoing_items) # Pass the list of events to the callback
-            @buffer_state[:network_down] = false # Reset network status after successful flush
-            flush_buffer_files # Flush buffer files if any exist
+            @buffer_state[:last_flush] = Time.now.to_i
+            @buffer_config[:logger].info("Flush completed. Flushed #{outgoing_items.size} events, #{outgoing_size} bytes")
           rescue => e
-            @buffer_config[:logger].error("Flush failed: #{e.message}")
-            @buffer_state[:network_down] = true
-            save_buffer_to_file(outgoing_items)
+            retries += 1
+            if retries <= @buffer_config[:max_retries]
+              @buffer_config[:logger].error("Flush failed: #{e.message}. \nRetrying (#{retries}/#{@buffer_config[:max_retries]})...")
+              sleep 1
+              retry
+            else
+              @buffer_config[:logger].error("Max retries reached. Failed to flush #{outgoing_items.size} items and #{outgoing_size} bytes")
+              handle_failed_flush(outgoing_items, e.message)
+            end
           end
 
-          @buffer_state[:last_flush] = Time.now.to_i
-          @buffer_config[:logger].info("Flush completed. Flushed #{outgoing_items.size} events, #{outgoing_size} bytes")
-
-          items_flushed = outgoing_items.size
         ensure
           @flush_mutex.unlock
         end
-
-					items_flushed
-				end
-
-      def save_buffer_to_file(events)
-        buffer_state_copy = {
-          pending_items: events,
-          pending_size: events.sum(&:bytesize)
-        }
-
-        ::FileUtils.mkdir_p(@buffer_config[:buffer_dir]) # Ensure directory exists
-        file_path = ::File.join(@buffer_config[:buffer_dir], "buffer_state_#{Time.now.to_i}_#{SecureRandom.uuid}.log")
-        ::File.open(file_path, 'w') do |file|
-          file.write(Marshal.dump(buffer_state_copy))
-        end
-        @buffer_config[:logger].info("Saved buffer state to file: #{file_path}")
       end
 
-      def load_buffer_from_files
-        Dir.glob(::File.join(@buffer_config[:buffer_dir], 'buffer_state_*.log')).each do |file_path|
+      def handle_failed_flush(items, error_message)
+        if @buffer_config[:failed_items_path]
           begin
-            buffer_state = Marshal.load(::File.read(file_path))
-            @buffer_state[:pending_items].concat(buffer_state[:pending_items])
-            @buffer_state[:pending_size] += buffer_state[:pending_size]
-            ::File.delete(file_path)
+            ::File.open(@buffer_config[:failed_items_path], 'a') do |file|
+              items.each do |item|
+                file.puts(item)
+              end
+            end
+            @buffer_config[:logger].info("Failed items stored in #{@buffer_config[:failed_items_path]}")
           rescue => e
-            @buffer_config[:logger].error("Failed to load buffer from file: #{e.message}")
+            @buffer_config[:logger].error("Failed to store items: #{e.message}")
           end
-        end
-        @buffer_config[:logger].info("Loaded buffer state from files")
-      end
-
-      def flush_buffer_files
-        Dir.glob(::File.join(@buffer_config[:buffer_dir], 'buffer_state_*.log')).each do |file_path|
-          begin
-            buffer_state = Marshal.load(::File.read(file_path))
-            @buffer_config[:logger].info("Flushed from file: #{file_path}")
-            @flush_callback.call(buffer_state[:pending_items])
-            ::File.delete(file_path)
-            @buffer_config[:logger].info("Flushed and deleted buffer state file: #{file_path}")
-          rescue => e
-            @buffer_config[:logger].error("Failed to flush buffer state file: #{e.message}")
-            break
-          end
+        else
+          @buffer_config[:logger].warn("No failed_items_path configured. Data loss may occur.")
         end
       end
 
-      def clear_buffer_files
-        Dir.glob(::File.join(@buffer_config[:buffer_dir], 'buffer_state_*.log')).each do |file_path|
-          ::File.delete(file_path)
-        end
-        @buffer_config[:logger].info("Cleared all buffer state files")
+      def buffer_initialize
+        @buffer_state[:pending_items] = []
+        @buffer_state[:pending_size] = 0
       end
     end
   end
