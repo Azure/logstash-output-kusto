0609cc569a76ebfe68da35e8db14254fec773c44
ag-ramachandran <ramacg@microsoft.com>
2026-02-04 18:13:06 +0530

    *Add dynamic targets for data

--- spec/outputs/kusto/ingestor_spec.rb
+++ spec/outputs/kusto/ingestor_spec.rb
@@ -5,18 +5,15 @@ require 'logstash/outputs/kusto/ingestor'
 
 describe LogStash::Outputs::Kusto::Ingestor do
 
-  let(:ingest_url) { "https://ingest-sdkse2etest.eastus.kusto.windows.net/" }
+  let(:ingest_url) { "https://ingest-sdktestcluster.southeastasia.dev.kusto.windows.net/" }
   let(:app_id) { "myid" }
   let(:app_key) { LogStash::Util::Password.new("mykey") }
   let(:app_tenant) { "mytenant" }
-  let(:managed_identity) { "managed_identity" }  
-  let(:database) { "mydatabase" }
+  let(:managed_identity) { "managed_identity" }
   let(:cliauth) { false }
-  let(:table) { "mytable" }
   let(:proxy_host) { "localhost" }
   let(:proxy_port) { 80 }
   let(:proxy_protocol) { "http" }
-  let(:json_mapping) { "mymapping" }
   let(:delete_local) { false }
   let(:logger) { spy('logger') }
 
@@ -25,65 +22,78 @@ describe LogStash::Outputs::Kusto::Ingestor do
     it 'does not throw an error when initializing' do
       # note that this will cause an internal error since connection is being tried.
       # however we still want to test that all the java stuff is working as expected
-      expect { 
-        ingestor = described_class.new(ingest_url, app_id, app_key, app_tenant, managed_identity, cliauth, database, table, json_mapping, delete_local, proxy_host, proxy_port,proxy_protocol, logger)
+      expect {
+        ingestor = described_class.new(ingest_url, app_id, app_key, app_tenant, managed_identity, cliauth, delete_local, proxy_host, proxy_port, proxy_protocol, logger)
         ingestor.stop
       }.not_to raise_error
-    end
-    
-    dynamic_name_array = ['/a%{name}/', '/a %{name}/', '/a- %{name}/', '/a- %{name}']
-
-    context 'doesnt allow database to have some dynamic part' do
-      dynamic_name_array.each do |test_database|
-        it "with database: #{test_database}" do
-          expect {
-            ingestor = described_class.new(ingest_url, app_id, app_key, app_tenant, managed_identity, cliauth, test_database, table, json_mapping, delete_local, proxy_host, proxy_port,proxy_protocol,logger)
-            ingestor.stop
-          }.to raise_error(LogStash::ConfigurationError)          
-        end
-      end
-    end
-
-    context 'doesnt allow table to have some dynamic part' do
-      dynamic_name_array.each do |test_table|
-        it "with database: #{test_table}" do
-          expect {
-            ingestor = described_class.new(ingest_url, app_id, app_key, app_tenant, managed_identity, cliauth, database, test_table, json_mapping, delete_local, proxy_host, proxy_port,proxy_protocol,logger)
-            ingestor.stop
-          }.to raise_error(LogStash::ConfigurationError)          
-        end
-      end
-    end
-
-    context 'doesnt allow mapping to have some dynamic part' do
-      dynamic_name_array.each do |json_mapping|
-        it "with database: #{json_mapping}" do
-          expect {
-            ingestor = described_class.new(ingest_url, app_id, app_key, app_tenant, managed_identity, cliauth, database, table, json_mapping, delete_local, proxy_host, proxy_port,proxy_protocol,logger)
-            ingestor.stop
-          }.to raise_error(LogStash::ConfigurationError)          
-        end
-      end
     end
 
     context 'proxy protocol has to be http or https' do
       it "with proxy protocol: socks" do
         expect {
-          ingestor = described_class.new(ingest_url, app_id, app_key, app_tenant, managed_identity, cliauth, database, table, json_mapping, delete_local, proxy_host, proxy_port,'socks',logger)
+          ingestor = described_class.new(ingest_url, app_id, app_key, app_tenant, managed_identity, cliauth, delete_local, proxy_host, proxy_port, 'socks', logger)
           ingestor.stop
-        }.to raise_error(LogStash::ConfigurationError)          
+        }.to raise_error(LogStash::ConfigurationError)
       end
     end
 
     context 'one of appid or managedid has to be provided' do
       it "with empty managed identity and appid" do
         expect {
-          ingestor = described_class.new(ingest_url, "", app_key, app_tenant, "", cliauth, database, table, json_mapping, delete_local, proxy_host, proxy_port,'socks',logger)
+          ingestor = described_class.new(ingest_url, "", app_key, app_tenant, "", cliauth, delete_local, proxy_host, proxy_port, 'http', logger)
           ingestor.stop
-        }.to raise_error(LogStash::ConfigurationError)          
+        }.to raise_error(LogStash::ConfigurationError)
+      end
+    end
+  end
+
+  describe '#extract_metadata_from_path' do
+    let(:ingestor) do
+      described_class.new(ingest_url, app_id, app_key, app_tenant, managed_identity, cliauth, delete_local, proxy_host, proxy_port, proxy_protocol, logger)
+    end
+
+    after(:each) do
+      ingestor.stop
+    end
+
+    context 'with database.table format' do
+      it 'extracts database and table' do
+        path = '/tmp/logstash.mydatabase.mytable'
+        metadata = ingestor.send(:extract_metadata_from_path, path)
+        expect(metadata[:database]).to eq('mydatabase')
+        expect(metadata[:table]).to eq('mytable')
+        expect(metadata[:mapping]).to be_nil
       end
     end
 
+    context 'with database.table.mapping format' do
+      it 'extracts database, table and mapping' do
+        path = '/tmp/logstash.mydatabase.mytable.mymapping'
+        metadata = ingestor.send(:extract_metadata_from_path, path)
+        expect(metadata[:database]).to eq('mydatabase')
+        expect(metadata[:table]).to eq('mytable')
+        expect(metadata[:mapping]).to eq('mymapping')
+      end
+    end
+
+    context 'with complex path' do
+      it 'extracts from filename only' do
+        path = '/var/log/app-2024-01-01.mydatabase.mytable.mymapping'
+        metadata = ingestor.send(:extract_metadata_from_path, path)
+        expect(metadata[:database]).to eq('mydatabase')
+        expect(metadata[:table]).to eq('mytable')
+        expect(metadata[:mapping]).to eq('mymapping')
+      end
+    end
+
+    context 'with invalid format' do
+      it 'raises error for single part filename' do
+        path = '/tmp/logstash'
+        expect {
+          ingestor.send(:extract_metadata_from_path, path)
+        }.to raise_error(LogStash::ConfigurationError)
+      end
+    end
   end
 
   # describe 'receiving events' do
